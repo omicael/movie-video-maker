ff64a0242b7447902199e8383d259e2d6169c2aa
Micael <micael.sax@hotmail.com>
2019-05-18 08:46:53 -0300

    developed at school in my free time

--- index.js
+++ index.js
@@ -3,37 +3,72 @@ const robots = {
     //userInput: require('./robots/user-input.js'),
     fetchMoviesList: require('./robots/fetch-movies-list.js'),
     state: require('./robots/state.js'),
-    movieRobot: require('./robots/movie-robot.js'),
-    imageRobot: require('./robots/image.js')
+    movie: require('./robots/movie.js'),
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js'),
+    youtube: require('./robots/youtube.js')
 }
 
 
 function start() {
     const moviesList = []
+    const limits = {}
+
+    /* gets limits from args */
+    process.argv.length > 2 ? (limits.bottom = Math.ceil(process.argv[2])) : (limits.bottom = 0)
+    process.argv.length > 3 ? (limits.upper = Math.ceil(process.argv[3])) : (limits.upper = Math.ceil(limits.bottom) + 1000)
+    
 
     if (!robots.state.fileExists(robots.settings.moviesPath + robots.settings.moviesListFilePath)) {
-        console.log('\nBuscando lista de filmes')
+        console.log('\nFetching movies list')
         robots.fetchMoviesList(moviesList, function(moviesList) {  
             robots.state.save(moviesList, robots.settings.moviesPath + robots.settings.moviesListFilePath)
             orchestrator(moviesList)
         })
     } 
     else {
-        console.log('\nLendo lista de filmes')
+        console.log('\nLoading movies list')
         orchestrator(robots.state.load(robots.settings.moviesPath + robots.settings.moviesListFilePath))
     }
 
     async function orchestrator(moviesList) {
-        cont = 1
-        console.log('\nFilme nº ' + cont++)
-        
-        //const movieContent = await robots.movieRobot.fetchMovieInTMDB(moviesList[1].id)
-        const movieContent = robots.movieRobot.loadMovieData(moviesList[1].id)
-        await robots.imageRobot(movieContent)
-
-        
+        for(let cont = limits.bottom; cont < limits.upper; cont++) {
+            console.log(`\nMovie nº ${cont}`)
+            
+            const movieContent = await robots.movie.fetchMovieInTMDB(moviesList[cont].id)
+            //const movieContent = robots.movie.loadMovieData(moviesList[cont].id)
+            await robots.image(movieContent)
+            await robots.video(movieContent)
+            //await robots.youtube(movieContent)
+        }
     }
     
 }
 
+showOrDisableLogMessages(robots.settings.showLogMessages)
 start()
+
+function showOrDisableLogMessages (show) {
+    const fs = require('fs')
+    const data = fs.readFileSync('/home/midas/Área de Trabalho/micael/06 Rodando com FMA/fma_genre/genre_small/list.tr', 'utf8').toString().split('\n')
+    const limits = {}
+
+    /* gets limits from args */
+    process.argv.length > 2 ? (limits.bottom = Math.ceil(process.argv[2])) : (limits.bottom = 0)
+    process.argv.length > 3 ? (limits.upper = Math.ceil(process.argv[3])) : (limits.upper = Math.ceil(limits.bottom) + 1000)
+    var cont = limits.bottom
+
+    const oldConsole = console.log
+    
+    if (!show) {
+        console.log = function (message) {
+            if (message[1] == 'M') { 
+                oldConsole(`- processando os áudios e gerando as features - ${cont} de 6400.. /home/midas/Área de Trabalho/micael/06 Rodando com FMA/fma_genre/genre_small/${data[cont]}`)
+                cont++
+            }
+            // else if (message[0] == '<') {
+            //     oldConsole(message)
+            // }
+        }
+    }
+}
--- robots/image.js
+++ robots/image.js
@@ -1,27 +1,59 @@
 const imageDownloader = require('image-downloader')
 const settings = require('../settings/general-settings.json')
 const fs = require('fs')
+const path = require('path')
 
 async function robot(movieContent) {
-    creatingFolder(settings.moviesPath + movieContent.id + '/images/')
+    await creatingFolder(path.normalize(__dirname + '/../' + settings.moviesPath + movieContent.id + '/images/'))
+    console.log('< - acabou a funcao')
     await downloadAllImages(movieContent)
-    console.log('> download das imagens concluido')
-
-    function creatingFolder(filePath) {
-        if (!fs.existsSync(filePath)) {
+    
+    async function creatingFolder(filePath) {
+        await new Promise((resolve, reject) => { 
             fs.mkdir(filePath.substring(0, filePath.lastIndexOf('/')), { recursive: true }, (err) => {
-                if (err) throw err;
+                if (err) {
+                    if (err.code == 'EEXIST') {
+                        /* pasta ja existia */
+                        console.log('< - pasta existia')
+                        resolve()
+                    }
+                    else {
+                        console.log('erro na criação da pasta images')
+                        console.log(err)
+                        throw err
+                        reject()
+                    }
+                }
+                else {
+                    console.log('< - pasta criada')
+                    resolve()
+                }
             });
-            console.log('pasta criada')
-        }
+        })
     }
 
     async function downloadAllImages(movieContent) {
-        for (let imageIndex = 0; imageIndex < movieContent.images.length; imageIndex++){
-            try {
-                await downloadAndSave(settings.TMDBimagePath + movieContent.images[imageIndex], settings.moviesPath + movieContent.id + '/images/' + imageIndex + '-original.png')
-            } catch (error) {
-                console.log(`> [${imageIndex}] Erro ao baixar (${movieContent.images[imageIndex]}): ${error}`)
+        console.log('> Downloading images')
+
+        /* trata se não encontrar nenhuma imagem */
+        if (movieContent.images.length == 0) {
+            movieContent.images.push(settings.video.blackImage)
+        }
+
+
+        for (let imageIndex = 0; imageIndex < settings.quantityOfImages; imageIndex++){
+            const baseImageUrl = (movieContent.images[imageIndex % movieContent.images.length][0] == '/') ? settings.TMDBimagePath : ''
+            const imageUrl = baseImageUrl + movieContent.images[imageIndex % movieContent.images.length]
+            
+            var erro = true
+            for (let tentativa = 0; tentativa < 11 && erro; tentativa++) {
+                try {
+                    await downloadAndSave(imageUrl, settings.moviesPath + movieContent.id + '/images/' + imageIndex + '-original.png')
+                    erro = false
+                } catch (error) {
+                    console.log(`> [${imageIndex}] Erro ao baixar (${movieContent.images[imageIndex % movieContent.images.length]}): ${error} - Tentativa [${tentativa}]`)
+                    tentativa++
+                }
             }
         }
     }
